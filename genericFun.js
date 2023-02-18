547fa47f0f8cb148a85e249e50be0af17fd2a46b
Giacca <matteo.giaccaglia03@gmail.com>
2023-02-18 01:48:37 +0100

    update

--- genericFun.js
+++ genericFun.js
@@ -23,3 +23,29 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '<a class="btn btn-primary mt-2 border-0" href="index.html"><span class="center">Torna alla home</span></a>    ',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Esperienza aggiunta con successo!', 'success')
+  })
+}
